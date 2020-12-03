71b50f3e4416ee3cf6a1a3d742cbe14769cd86df
Nick Meyer <nick@e3b0c442.io>
2020-12-03 07:09:41 -0600

    2020 Javascript day 3

---
+++ 2020/javascript/day3.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import fs from "fs";
+
+const slope_trees = (rows, slope) => {
+  const [r, d] = slope;
+  let x = 0;
+  let trees = 0;
+  for (let i = 0; i < rows.length; i += d) {
+    if (x >= rows[i].length) {
+      x = x - rows[i].length;
+    }
+    if (rows[i][x] === "#") {
+      trees++;
+    }
+    x += r;
+  }
+  return trees;
+};
+const part1 = (input) => slope_trees(input.split("\n"), [3, 1]);
+
+const part2 = (input) =>
+  [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ].reduce((acc, cur) => acc * slope_trees(input.split("\n"), cur), 1);
+
+const day3 = (inputFile) => {
+  const input = fs.readFileSync(inputFile, "utf8");
+
+  console.log("Day 3: Toboggan Trajectory");
+  console.log(`\tPart 1: ${part1(input)}`);
+  console.log(`\tPart 2: ${part2(input)}`);
+};
+
+export default day3;
+
+try {
+  day3(process.argv[2]);
+} catch (e) {
+  console.log(`${e}`);
+}
