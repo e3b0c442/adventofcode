f54071d18b53b5be7a99ba6128b508016afd6b30
Nick Meyer <nick@e3b0c442.io>
2020-12-19 08:35:58 -0600

    2015 Rust day 10

---
+++ 2015/rust/src/day10.rs
@@ -0,0 +1,61 @@
+use simple_error::require_with;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn day10(input_file: &str) -> Result<(), Box<dyn Error>> {
+    println!("Day 10: Elves Look, Elves Say");
+
+    let mut f = File::open(input_file)?;
+    let mut input = String::new();
+    f.read_to_string(&mut input)?;
+
+    let (sol1, cont) = part1(&input)?;
+    println!("\tPart 1: {}", sol1);
+    println!("\tPart 2: {}", part2(&cont)?);
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<(i32, String), Box<dyn Error>> {
+    let mut input = input.to_owned();
+    for _ in 0..40 {
+        let mut output = String::new();
+        let mut iter = input.chars();
+        let mut last = require_with!(iter.next(), &format!("Invalid input: {}", input));
+        let mut count = 1;
+        while let Some(num) = iter.next() {
+            if num == last {
+                count += 1;
+            } else {
+                output.push_str(&format!("{}{}", count, last));
+                count = 1;
+                last = num;
+            }
+        }
+        output.push_str(&format!("{}{}", count, last));
+        input = output;
+    }
+    return Ok((input.len() as i32, input));
+}
+
+fn part2(input: &str) -> Result<i32, Box<dyn Error>> {
+    let mut input = input.to_owned();
+    for _ in 40..50 {
+        let mut output = String::new();
+        let mut iter = input.chars();
+        let mut last = require_with!(iter.next(), &format!("Invalid input: {}", input));
+        let mut count = 1;
+        while let Some(num) = iter.next() {
+            if num == last {
+                count += 1;
+            } else {
+                output.push_str(&format!("{}{}", count, last));
+                count = 1;
+                last = num;
+            }
+        }
+        output.push_str(&format!("{}{}", count, last));
+        input = output;
+    }
+    return Ok(input.len() as i32);
+}
--- 2015/rust/src/lib.rs
+++ 2015/rust/src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
 mod day1;
+mod day10;
 mod day2;
 mod day3;
 mod day4;
@@ -10,6 +11,7 @@ mod day7;
 mod day8;
 mod day9;
 pub use day1::day1;
+pub use day10::day10;
 pub use day2::day2;
 pub use day3::day3;
 pub use day4::day4;
@@ -20,7 +22,7 @@ pub use day8::day8;
 pub use day9::day9;
 
 pub fn run_all(input_path: &str) -> Result<(), Box<dyn Error>> {
-    let funcs = [day1, day2, day3, day4, day5, day6, day7, day8, day9];
+    let funcs = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10];
     for (i, func) in funcs.iter().enumerate() {
         func(&format!("{}/{}.txt", input_path, i + 1))?;
     }
