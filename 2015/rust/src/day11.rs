74dc28bffbea2c1175994925fe4318f30b14011d
Nick Meyer <nick@e3b0c442.io>
2020-12-19 09:50:29 -0600

    2015 Rust day 11

---
+++ 2015/rust/src/day11.rs
@@ -0,0 +1,86 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn day11(input_file: &str) -> Result<(), Box<dyn Error>> {
+    println!("Day 11: Corporate Policy");
+
+    let mut f = File::open(input_file)?;
+    let mut input = String::new();
+    f.read_to_string(&mut input)?;
+
+    let sol1 = part1(&input)?;
+    println!("\tPart 1: {}", sol1);
+    println!("\tPart 2: {}", part2(&sol1)?);
+    Ok(())
+}
+
+fn increment(input: &mut Vec<u8>) {
+    for c in input.into_iter().rev() {
+        if char::from(*c) == 'z' {
+            let mut b: [u8; 1] = [0];
+            'a'.encode_utf8(&mut b[..]);
+            *c = b[0];
+        } else {
+            *c += 1;
+            return;
+        }
+    }
+}
+
+fn part1(input: &str) -> Result<String, Box<dyn Error>> {
+    let mut input: Vec<u8> = Vec::from(input.as_bytes());
+    loop {
+        let (mut one_ago, mut two_ago, mut three_ago) = (0, 0, 0);
+        let (mut pairs, mut triplet) = (0, false);
+        for uc in input.iter() {
+            let c = char::from(*uc);
+            match c {
+                'i' | 'o' | 'l' => break,
+                _ => (),
+            }
+            if *uc as i32 - one_ago as i32 == 1 && one_ago as i32 - two_ago as i32 == 1 {
+                triplet = true;
+            }
+            if *uc == one_ago && (*uc != two_ago || (two_ago == three_ago)) {
+                pairs += 1;
+            }
+            if triplet && pairs >= 2 {
+                return Ok(String::from_utf8(input)?);
+            }
+            three_ago = two_ago;
+            two_ago = one_ago;
+            one_ago = *uc;
+        }
+        increment(&mut input);
+    }
+}
+
+fn part2(input: &str) -> Result<String, Box<dyn Error>> {
+    let mut input: Vec<u8> = Vec::from(input.as_bytes());
+    increment(&mut input);
+    loop {
+        let (mut one_ago, mut two_ago, mut three_ago) = (0, 0, 0);
+        let (mut pairs, mut triplet) = (0, false);
+        for uc in input.iter() {
+            let c = char::from(*uc);
+            match c {
+                'i' | 'o' | 'l' => break,
+                _ => (),
+            }
+            if *uc as i32 - one_ago as i32 == 1 && one_ago as i32 - two_ago as i32 == 1 {
+                triplet = true;
+            }
+            if *uc == one_ago && (*uc != two_ago || (two_ago == three_ago)) {
+                pairs += 1;
+            }
+            if triplet && pairs >= 2 {
+                return Ok(String::from_utf8(input)?);
+            }
+            three_ago = two_ago;
+            two_ago = one_ago;
+            one_ago = *uc;
+        }
+        increment(&mut input);
+    }
+}
--- 2015/rust/src/lib.rs
+++ 2015/rust/src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 mod day1;
 mod day10;
+mod day11;
 mod day2;
 mod day3;
 mod day4;
@@ -12,6 +13,7 @@ mod day8;
 mod day9;
 pub use day1::day1;
 pub use day10::day10;
+pub use day11::day11;
 pub use day2::day2;
 pub use day3::day3;
 pub use day4::day4;
@@ -22,7 +24,9 @@ pub use day8::day8;
 pub use day9::day9;
 
 pub fn run_all(input_path: &str) -> Result<(), Box<dyn Error>> {
-    let funcs = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10];
+    let funcs = [
+        day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11,
+    ];
     for (i, func) in funcs.iter().enumerate() {
         func(&format!("{}/{}.txt", input_path, i + 1))?;
     }
