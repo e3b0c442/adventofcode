763539842336c26dde8695c95530e62a670cd0bd
Nick Meyer <nick@e3b0c442.io>
2020-12-19 00:21:00 -0600

    2015 Rust day 9

---
+++ 2015/rust/src/day9.rs
@@ -0,0 +1,105 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use simple_error::require_with;
+use std::collections::{HashMap, HashSet};
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn day9(input_file: &str) -> Result<(), Box<dyn Error>> {
+    println!("Day 9: All in a Single Night");
+
+    let mut f = File::open(input_file)?;
+    let mut input = String::new();
+    f.read_to_string(&mut input)?;
+
+    println!("\tPart 1: {}", part1(&input)?);
+    println!("\tPart 2: {}", part2(&input)?);
+    Ok(())
+}
+
+lazy_static! {
+    static ref DIST_RE: Regex = Regex::new(r"([A-Za-z]+) to ([A-Za-z]+) = (\d+)").unwrap();
+}
+
+fn input_to_map(input: &str) -> Result<HashMap<&str, HashMap<&str, i32>>, Box<dyn Error>> {
+    let mut map = HashMap::new();
+    for line in input.lines() {
+        let caps = require_with!(DIST_RE.captures(line), &format!("Invalid input: {}", line));
+        let (orig, dest, dist) = (
+            require_with!(caps.get(1), &format!("Invalid input: {}", line)).as_str(),
+            require_with!(caps.get(2), &format!("Invalid input: {}", line)).as_str(),
+            require_with!(caps.get(3), &format!("Invalid input: {}", line))
+                .as_str()
+                .parse::<i32>()?,
+        );
+
+        let from = map.entry(orig).or_insert(HashMap::new());
+        from.insert(dest, dist);
+        let to = map.entry(dest).or_insert(HashMap::new());
+        to.insert(orig, dist);
+    }
+
+    return Ok(map);
+}
+
+fn find_path_lens(
+    dists: &HashMap<&str, HashMap<&str, i32>>,
+    origin: &str,
+    remaining: HashSet<&str>,
+    so_far: i32,
+) -> Result<Vec<i32>, Box<dyn Error>> {
+    if remaining.len() == 0 {
+        return Ok(vec![so_far]);
+    }
+
+    let mut result = vec![];
+    for dest in remaining.iter() {
+        let mut clone = remaining.clone();
+        clone.remove(dest);
+
+        let leg = *require_with!(
+            require_with!(
+                dists.get(origin),
+                &format!("{} to {} not found", origin, dest)
+            )
+            .get(dest),
+            &format!("{} to {} not fonud", origin, dest)
+        );
+
+        result.extend(find_path_lens(dists, dest, clone, so_far + leg)?);
+    }
+    Ok(result)
+}
+
+fn part1(input: &str) -> Result<i32, Box<dyn Error>> {
+    let map = input_to_map(input)?;
+    let cities = map.keys().map(|k| *k).collect::<HashSet<&str>>();
+    let mut paths = cities
+        .iter()
+        .try_fold::<_, _, Result<Vec<i32>, Box<dyn Error>>>(vec![], |mut sum, x| {
+            let mut clone = cities.clone();
+            clone.remove(x);
+            sum.extend(find_path_lens(&map, x, clone, 0)?);
+            Ok(sum)
+        })?;
+    paths.sort();
+
+    Ok(*paths.first().unwrap())
+}
+
+fn part2(input: &str) -> Result<i32, Box<dyn Error>> {
+    let map = input_to_map(input)?;
+    let cities = map.keys().map(|k| *k).collect::<HashSet<&str>>();
+    let mut paths = cities
+        .iter()
+        .try_fold::<_, _, Result<Vec<i32>, Box<dyn Error>>>(vec![], |mut sum, x| {
+            let mut clone = cities.clone();
+            clone.remove(x);
+            sum.extend(find_path_lens(&map, x, clone, 0)?);
+            Ok(sum)
+        })?;
+    paths.sort();
+
+    Ok(*paths.last().unwrap())
+}
--- 2015/rust/src/lib.rs
+++ 2015/rust/src/lib.rs
@@ -8,6 +8,7 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 pub use day1::day1;
 pub use day2::day2;
 pub use day3::day3;
@@ -16,9 +17,10 @@ pub use day5::day5;
 pub use day6::day6;
 pub use day7::day7;
 pub use day8::day8;
+pub use day9::day9;
 
 pub fn run_all(input_path: &str) -> Result<(), Box<dyn Error>> {
-    let funcs = [day1, day2, day3, day4, day5, day6, day7, day8];
+    let funcs = [day1, day2, day3, day4, day5, day6, day7, day8, day9];
     for (i, func) in funcs.iter().enumerate() {
         func(&format!("{}/{}.txt", input_path, i + 1))?;
     }
