02d6319b18bb20f8cb9f3a224b08d55a0944dff6
Nick Meyer <nick@e3b0c442.io>
2020-12-20 16:47:08 -0600

    2015 Rust day 16

---
+++ 2015/rust/src/day16.rs
@@ -0,0 +1,147 @@
+use simple_error::{bail, require_with};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn day16(input_file: &str) -> Result<(), Box<dyn Error>> {
+    println!("Day 16: Aunt Sue");
+
+    let mut f = File::open(input_file)?;
+    let mut input = String::new();
+    f.read_to_string(&mut input)?;
+
+    println!("\tPart 1: {}", part1(&input)?);
+    println!("\tPart 2: {}", part2(&input)?);
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Sue<'a> {
+    number: i32,
+    fields: HashMap<&'a str, i32>,
+}
+
+fn input_to_sues(input: &str) -> Result<Vec<Sue>, Box<dyn Error>> {
+    input
+        .lines()
+        .map(|line| {
+            let parts = line.splitn(2, ": ").collect::<Vec<&str>>();
+            let sue = require_with!(
+                require_with!(parts.get(0), &format!("Invalid input: {}", line))
+                    .strip_prefix("Sue "),
+                &format!("Invalid input: {}", line)
+            )
+            .parse::<i32>()?;
+
+            let fields = require_with!(parts.get(1), &format!("Invalid input: {}", line))
+                .split(", ")
+                .try_fold::<_, _, Result<HashMap<&str, i32>, Box<dyn Error>>>(
+                    HashMap::new(),
+                    |mut map, kva| {
+                        let kv = kva.split(": ").collect::<Vec<&str>>();
+                        map.insert(
+                            *require_with!(kv.get(0), &format!("Invalid input: {}", line)),
+                            require_with!(kv.get(1), &format!("Invalid input: {}", line))
+                                .parse::<i32>()?,
+                        );
+                        Ok(map)
+                    },
+                )?;
+
+            Ok(Sue {
+                number: sue,
+                fields: fields,
+            })
+        })
+        .collect()
+}
+
+fn filter_predicate(fields: &HashMap<&str, i32>, predicate: &[(&str, (&str, i32))]) -> bool {
+    match predicate
+        .iter()
+        .try_fold((), |_, (k, (op, v))| match fields.get(k) {
+            None => Ok(()),
+            Some(val) => match *op {
+                "eq" => {
+                    if val == v {
+                        Ok(())
+                    } else {
+                        Err(())
+                    }
+                }
+                "gt" => {
+                    if val > v {
+                        Ok(())
+                    } else {
+                        Err(())
+                    }
+                }
+                "lt" => {
+                    if val < v {
+                        Ok(())
+                    } else {
+                        Err(())
+                    }
+                }
+                _ => Err(()),
+            },
+        }) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+fn part1(input: &str) -> Result<i32, Box<dyn Error>> {
+    let sues = input_to_sues(input)?;
+    let predicate = [
+        ("children", ("eq", 3)),
+        ("cats", ("eq", 7)),
+        ("samoyeds", ("eq", 2)),
+        ("pomeranians", ("eq", 3)),
+        ("akitas", ("eq", 0)),
+        ("vizslas", ("eq", 0)),
+        ("goldfish", ("eq", 5)),
+        ("trees", ("eq", 3)),
+        ("cars", ("eq", 2)),
+        ("perfumes", ("eq", 1)),
+    ];
+
+    let filtered = sues
+        .into_iter()
+        .filter(|x| filter_predicate(&x.fields, &predicate))
+        .collect::<Vec<Sue>>();
+
+    if filtered.len() != 1 {
+        bail!("Expected 1 result, got {}", filtered.len());
+    }
+
+    Ok(filtered[0].number)
+}
+
+fn part2(input: &str) -> Result<i32, Box<dyn Error>> {
+    let sues = input_to_sues(input)?;
+    let predicate = [
+        ("children", ("eq", 3)),
+        ("cats", ("gt", 7)),
+        ("samoyeds", ("eq", 2)),
+        ("pomeranians", ("lt", 3)),
+        ("akitas", ("eq", 0)),
+        ("vizslas", ("eq", 0)),
+        ("goldfish", ("lt", 5)),
+        ("trees", ("gt", 3)),
+        ("cars", ("eq", 2)),
+        ("perfumes", ("eq", 1)),
+    ];
+
+    let filtered = sues
+        .into_iter()
+        .filter(|x| filter_predicate(&x.fields, &predicate))
+        .collect::<Vec<Sue>>();
+
+    if filtered.len() != 1 {
+        bail!("Expected 1 result, got {}", filtered.len());
+    }
+
+    Ok(filtered[0].number)
+}
--- 2015/rust/src/lib.rs
+++ 2015/rust/src/lib.rs
@@ -6,6 +6,8 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
+mod day16;
 mod day2;
 mod day3;
 mod day4;
@@ -14,13 +16,14 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
-mod day15;
 pub use day1::day1;
 pub use day10::day10;
 pub use day11::day11;
 pub use day12::day12;
 pub use day13::day13;
 pub use day14::day14;
+pub use day15::day15;
+pub use day16::day16;
 pub use day2::day2;
 pub use day3::day3;
 pub use day4::day4;
@@ -29,11 +32,11 @@ pub use day6::day6;
 pub use day7::day7;
 pub use day8::day8;
 pub use day9::day9;
-pub use day15::day15;
 
 pub fn run_all(input_path: &str) -> Result<(), Box<dyn Error>> {
     let funcs = [
-        day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15
+        day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14,
+        day15, day16,
     ];
     for (i, func) in funcs.iter().enumerate() {
         func(&format!("{}/{}.txt", input_path, i + 1))?;
