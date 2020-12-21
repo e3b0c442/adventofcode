e72c24ca4728ff8ae17dbc99f86e97f00c52ccbe
Nick Meyer <nick@e3b0c442.io>
2020-12-20 22:44:14 -0600

    2015 Rust day 17

---
+++ 2015/rust/src/day17.rs
@@ -0,0 +1,72 @@
+use std::cmp::min;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn day17(input_file: &str) -> Result<(), Box<dyn Error>> {
+    println!("Day 17: No Such Thing as Too Much");
+
+    let mut f = File::open(input_file)?;
+    let mut input = String::new();
+    f.read_to_string(&mut input)?;
+
+    let sol1 = part1(&input)?;
+    println!("\tPart 1: {}", sol1.0);
+    println!("\tPart 2: {}", part2(&input, sol1.1)?);
+    Ok(())
+}
+
+fn find_container_combos(
+    containers: Vec<i32>,
+    target: i32,
+    depth: i32,
+    target_depth: i32,
+    mut smallest: i32,
+) -> (i32, i32) {
+    if containers.len() == 0 || (target_depth > 0 && depth > target_depth) {
+        return (0, i32::MAX);
+    }
+
+    let mut found = 0;
+    for (i, c) in containers.iter().enumerate() {
+        if *c > target {
+            continue;
+        }
+        if *c == target && (target_depth <= 0 || depth + 1 == target_depth) {
+            found += 1;
+            smallest = depth + 1;
+            continue;
+        }
+        let mut containers = containers.clone();
+        containers.remove(i);
+        let containers = (&containers[i..]).iter().map(|x| *x).collect::<Vec<i32>>();
+        let (leg_found, leg_min) =
+            find_container_combos(containers, target - *c, depth + 1, target_depth, smallest);
+        found += leg_found;
+        smallest = min(smallest, leg_min);
+    }
+
+    (found, smallest)
+}
+
+fn part1(input: &str) -> Result<(i32, i32), Box<dyn Error>> {
+    let mut sizes = input
+        .lines()
+        .map(|x| x.parse::<i32>())
+        .collect::<Result<Vec<i32>, _>>()?;
+    sizes.sort();
+    sizes = sizes.into_iter().rev().collect();
+
+    Ok(find_container_combos(sizes, 150, 0, -1, i32::MAX))
+}
+
+fn part2(input: &str, size: i32) -> Result<i32, Box<dyn Error>> {
+    let mut sizes = input
+        .lines()
+        .map(|x| x.parse::<i32>())
+        .collect::<Result<Vec<i32>, _>>()?;
+    sizes.sort();
+    sizes = sizes.into_iter().rev().collect();
+
+    Ok(find_container_combos(sizes, 150, 0, size, i32::MAX).0)
+}
--- 2015/rust/src/lib.rs
+++ 2015/rust/src/lib.rs
@@ -8,6 +8,7 @@ mod day13;
 mod day14;
 mod day15;
 mod day16;
+mod day17;
 mod day2;
 mod day3;
 mod day4;
@@ -24,6 +25,7 @@ pub use day13::day13;
 pub use day14::day14;
 pub use day15::day15;
 pub use day16::day16;
+pub use day17::day17;
 pub use day2::day2;
 pub use day3::day3;
 pub use day4::day4;
@@ -36,7 +38,7 @@ pub use day9::day9;
 pub fn run_all(input_path: &str) -> Result<(), Box<dyn Error>> {
     let funcs = [
         day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14,
-        day15, day16,
+        day15, day16, day17,
     ];
     for (i, func) in funcs.iter().enumerate() {
         func(&format!("{}/{}.txt", input_path, i + 1))?;
